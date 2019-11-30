2105c26fbf689a897eb8472bbdc46624402eb904
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-11-30 18:35:29 +0800

    some revisions

--- C files/prime.c
+++ C files/prime.c
@@ -1,29 +1,32 @@
 //Program that print prime numbers range from n1 to n2
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
 int main()
 {
-        int i,n,n1,n2,tmp,sqareroot;
-        printf("please enter n1 nad n2:");
-        scanf("%d,%d",&n1, &n2);
-        
-		else
-			if(n1>n2){
+	int i, n, n1, n2, tmp, t;
+	char c = 'y';
+	while (c == 'y' || c == 'Y') {
+		printf("Please enter n1 and n2:");
+		scanf("%d,%d", &n1, &n2);
+		if (n1 > n2) {
 			tmp = n1; n1 = n2; n2 = tmp;
 		}
-
 		for (n = n1; n <= n2; ++n) {
-			sqareroot=sqrt(n);		
-			for (i = 2; i < sqareroot; ++i) {
+			t = n / 2;
+			for (i = 2; i < t; ++i) {
 				if (n%i == 0) {
 					break;
 				}
 			}
-			if(i>=sqareroot)
-				printf("%d\t\t\t",n); 
-
+			if (i >= t)
+				printf("%d\t", n);
 		}
-        		
-		return 0;  
+		putchar(10);
+		putchar(10);
+		while ((c = getchar()) != '\n');
+		printf("Would you like to do it again?(y or n)\n");		
+		c=getchar();
+		putchar(10);
+	}
+
+	return 0;
 }
